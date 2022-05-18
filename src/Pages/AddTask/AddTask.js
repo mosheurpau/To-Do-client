ce5f6aa35787c86a22969075860980e45d4bb7f7
mosheurpau <mosheur.pau@gamil.com>
2022-05-18 23:51:27 +0600

    create all components file

---
+++ src/Pages/AddTask/AddTask.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <div>
+      <h2>Add Your Task</h2>
+    </div>
+  );
+};
+
+export default AddTask;
---
+++ src/Pages/Home/Task.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Task = () => {
+  return (
+    <div>
+      <h2>My Task</h2>
+    </div>
+  );
+};
+
+export default Task;
