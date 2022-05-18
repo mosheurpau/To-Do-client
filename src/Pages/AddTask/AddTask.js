3fa7f2ae65eb225acc10fcdb7c3153e35282ae2f
mosheurpau <mosheur.pau@gamil.com>
2022-05-19 03:45:44 +0600

    added show task and delete task

--- src/Pages/AddTask/AddTask.js
+++ src/Pages/AddTask/AddTask.js
@@ -1,10 +1,68 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import "./AddTask.css";
+
+// todo_admin
 
 const AddTask = () => {
+  const [user] = useAuthState(auth);
+  console.log(user.email);
+  const { register, handleSubmit } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    data.email = user.email;
+    const url = `http://localhost:5000/task`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+
+    navigate("/");
+  };
   return (
-    <div>
-      <h2>Add Your Task</h2>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center my-3">
+        <Col
+          xs="11"
+          lg="6"
+          className="mt-3 mx-auto bg-light rounded-top px-5 mb-3 py-5"
+        >
+          <h2 className="mb-3">
+            Add New <span style={{ color: "#EB7700" }}>Task</span>
+          </h2>
+          <form
+            className="d-flex flex-column"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              className="mb-2 item-input"
+              placeholder="Task Name"
+              type="text"
+              {...register("name", { required: true, maxLength: 100 })}
+            />
+            <textarea
+              className="mb-2 item-input"
+              placeholder="Description"
+              {...register("description")}
+            />
+            <input className="add-btn" type="submit" value="Add Task" />
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Pages/Home/Task.js
+++ src/Pages/Home/Task.js
@@ -1,10 +1,84 @@
-import React from "react";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Container, Table } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import "./Task.css";
 
 const Task = () => {
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      const url = `http://localhost:5000/task/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = tasks.filter((task) => task._id !== id);
+          setTasks(remaining);
+        });
+    }
+  };
+
+  const [user] = useAuthState(auth);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/task/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, [user]);
+  console.log(tasks);
+
+  const navigate = useNavigate();
+  const navigateToItemCompleted = (_id) => {
+    navigate(`/tasks/${_id}`);
+  };
   return (
-    <div>
-      <h2>My Task</h2>
-    </div>
+    <Container>
+      <div className="w-100 mx-auto mb-5">
+        <h2 className="my-5">Manage My Tasks</h2>
+        <Table striped bordered responsive hover variant="light">
+          <thead>
+            <tr>
+              <th>#ID</th>
+              <th>Task Name</th>
+              <th>Task Description</th>
+              <th>Task Completed</th>
+              <th>Task Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task._id}>
+                <td>...{task._id.slice(20, 30)}</td>
+                <td>{task.name}</td>
+                <td> {task.description}</td>
+                <td>
+                  <button
+                    className="update-btn"
+                    onClick={() => navigateToItemCompleted(task._id)}
+                  >
+                    Completed
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="delete-btn"
+                    onClick={() => handleDelete(task._id)}
+                  >
+                    <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </Container>
   );
 };
 
